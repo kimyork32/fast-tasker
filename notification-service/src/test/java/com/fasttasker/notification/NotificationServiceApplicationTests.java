65e8f56bd9716a3d90d4e9029e30ab856a6efea4
kimyork32 <CaronteGG.20851@gmail.com>
2026-01-13 10:13:24 -0500

    gather the application context for integration tests

--- notification-service/src/test/java/com/fasttasker/notification/NotificationServiceApplicationTests.java
+++ notification-service/src/test/java/com/fasttasker/notification/NotificationServiceApplicationTests.java
@@ -2,9 +2,14 @@ package com.fasttasker.notification;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 
-@SpringBootTest()
+@SpringBootTest
 class NotificationServiceApplicationTests {
+
+    @MockBean
+    private SimpMessagingTemplate simpMessagingTemplate;
 
 	@Test
 	void contextLoads() {
