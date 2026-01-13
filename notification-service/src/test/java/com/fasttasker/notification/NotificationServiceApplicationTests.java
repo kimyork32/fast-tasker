67c8b5d829117d9397247125b8d73e9f5d3c21a1
kimyork32 <CaronteGG.20851@gmail.com>
2026-01-13 10:32:53 -0500

    add properties in SpringBootTest annotation

--- notification-service/src/test/java/com/fasttasker/notification/NotificationServiceApplicationTests.java
+++ notification-service/src/test/java/com/fasttasker/notification/NotificationServiceApplicationTests.java
@@ -4,8 +4,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+        "spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration,org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration"
+})
+@ActiveProfiles("test")
 class NotificationServiceApplicationTests {
 
     @MockBean
